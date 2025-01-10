bd9d93a0c2e8adedafba7e5ddcb971b5ba7f7199
shirodune <182082960+shirodune@users.noreply.github.com>
2025-01-11 07:14:40 +0800

    Finish Exercise 3.2

--- index.js
+++ index.js
@@ -28,6 +28,10 @@ app.get('/', (request, response) => {
   response.send('<h1>Hello World!</h1>')
 })
 
+app.get('/info', (request, response) => {
+  response.send(`<p>Phonebook has info for ${persons.length} people</p><p>${(new Date()).toString()}</p>`)
+})
+
 app.get('/api/persons', (request, response) => {
   response.json(persons)
 })
