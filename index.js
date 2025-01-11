1c908a3b7791764558434de8064e3f93c2c0d95c
shirodune <182082960+shirodune@users.noreply.github.com>
2025-01-11 10:02:24 +0800

    Finish Exercise 3.3-3.6

--- index.js
+++ index.js
@@ -55,6 +55,12 @@ app.post('/api/persons', (request, response) => {
   const body = request.body
   console.log(body);
   
+  if(!body.name || !body.number) {
+    return response.status(400).json({
+      error: 'content missing'
+    })
+  }
+
   const person = {
     name: body.name,
     number: body.number,
