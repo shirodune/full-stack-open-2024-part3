7eb33a343a4806c30390b108d9e05dd68f31e38b
shirodune <182082960+shirodune@users.noreply.github.com>
2025-01-14 23:52:36 +0800

    Finish Exercise 3.7-3.11

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
+var morgan = require('morgan')
 
+morgan.token('body', function (req, res) { return JSON.stringify(req.body) })
+
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
+app.use(express.static('dist'))
 app.use(cors())
 app.use(express.json())
+
 
 let persons = [
   { 
@@ -81,7 +87,7 @@ app.delete('/api/persons/:id', (request, response) => {
   response.status(204).end()
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
